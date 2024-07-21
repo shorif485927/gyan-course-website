6cc681e49a79cd0512fc3f122dffd97b2b479fae
md shorif <mdshorifuddin463@gmail.com>
2024-07-22 01:44:20 +0600

    jequery include course  section

--- asset/js/web.js
+++ asset/js/web.js
@@ -1,3 +1,36 @@
+
+//    ! JQUERY CODE  SATARTS 
+
+      
+$(function(){
+
+
+      //   * courses  section
+
+      
+    $('.coures_card_slider').slick({
+       
+       slidesToShow:3,
+       slidesToScroll:1,
+      prevArrow:'.right__arrow',
+      nextArrow:'.left__arrow',
+      infinite:true,
+      // dots: true,
+      // dotsClass: " banner__dots"
+    });
+
+
+
+ })
+
+
+
+// ! JQUERY CODE ENDS 
+
+
+
+
+
 let GyanCounters = document.querySelectorAll('.community__card h4');
 
 
@@ -20,3 +53,27 @@ let GyanCounters = document.querySelectorAll('.community__card h4');
 
 
    });
+
+
+
+
+
+   let submit = document.querySelector('.submit');
+
+   let rform = document.querySelector ('.wrapper');
+
+   let popUP = document.querySelector('.popup');
+
+
+
+
+   submit.addEventListener('click', ()=>{
+        rform.classList.add('none');
+
+        popUP.classList.add('active')
+
+     
+   })
+
+
+
